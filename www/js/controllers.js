8e4b2b40c450468866276fc125593a2f394e4601
Eve Cho <regicidalryuu@gmail.com>
2016-08-01 21:29:16 -0400

    Adding new files

---
+++ www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('temperatureCtrl', function($scope) {
+
+})
+   
+.controller('settingsCtrl', function($scope) {
+
+})
+      
+.controller('suggestionsCtrl', function($scope) {
+
+})
+   
+.controller('personalPreferencesCtrl', function($scope) {
+
+})
+ 
